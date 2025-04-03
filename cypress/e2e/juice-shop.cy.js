3f7bafcf2b5e8303729112dcfa5af31eaa61ee85
unknown <emirhan.solak@ogr.dpu.edu.tr>
2025-04-03 15:04:53 +0300

    newest version

--- cypress/e2e/juice-shop.cy.js
+++ cypress/e2e/juice-shop.cy.js
@@ -90,65 +90,101 @@ describe("Juice-shop scenarios", () => {
       HomePage.reviewComment.should("contain.text", "K33p5 y0ur ju1cy 5plu773r 70 y0ur53lf!");
     });
 
-    // Create scenario - Add a review
-    // Click on search icon
-    // Search for Raspberry
-    // Select a product card - Raspberry Juice (1000ml)
-    // Type in review - "Tastes like metal"
-    // Click Submit
-    // Click expand reviews button/icon (wait for reviews to appear)
-    // Validate review -  "Tastes like metal"
+    it("Add a review", () => {
+      HomePage.searchIcon.click();
+      HomePage.searchInput.type("Raspberry{enter}");
+    
+      HomePage.productBox.contains("Raspberry Juice (1000ml)").click();
+    
+      HomePage.reviewTextArea.type("Tastes like metal");
+      HomePage.submitReviewButton.click();
+    
+      HomePage.expandReviewButton.click();
+      HomePage.reviewComment.should("contain.text", "Tastes like metal");
+    });
 
-    // Create scenario - Validate product card amount
-    // Validate that the default amount of cards is 12
-    // Change items per page (at the bottom of page) to 24
-    // Validate that the amount of cards is 24
-    // Change items per page (at the bottom of page) to 36
-    // Validate that the amount of cards is 35
+    it("Validate product card amount", () => {
+      HomePage.productBox.should("have.length", 12);
+    
+      HomePage.itemsPerPageDropdown.click();
+      HomePage.itemsPerPageOptions.contains("24").click();
+      HomePage.productBox.should("have.length", 24);
+    
+      HomePage.itemsPerPageDropdown.click();
+      HomePage.itemsPerPageOptions.contains("36").click();
+      HomePage.productBox.should("have.length", 35);
+    });
 
-    // Create scenario - Buy Girlie T-shirt
-    // Click on search icon
-    // Search for Girlie
-    // Add to basket "Girlie"
-    // Click on "Your Basket" button
-    // Create page object - BasketPage
-    // Click on "Checkout" button
-    // Create page object - SelectAddressPage
-    // Select address containing "United Fakedom"
-    // Click Continue button
-    // Create page object - DeliveryMethodPage
-    // Select delivery speed Standard Delivery
-    // Click Continue button
-    // Create page object - PaymentOptionsPage
-    // Select card that ends with "5678"
-    // Click Continue button
-    // Create page object - OrderSummaryPage
-    // Click on "Place your order and pay"
-    // Create page object - OrderCompletionPage
-    // Validate confirmation - "Thank you for your purchase!"
+    it("Buy Girlie T-shirt", () => {
+  HomePage.searchIcon.click();
+  HomePage.searchInput.type("Girlie{enter}");
 
-    // Create scenario - Add address
-    // Click on Account
-    // Click on Orders & Payment
-    // Click on My saved addresses
-    // Create page object - SavedAddressesPage
-    // Click on Add New Address
-    // Create page object - CreateAddressPage
-    // Fill in the necessary information
-    // Click Submit button
-    // Validate that previously added address is visible
+  
+  HomePage.productBox.contains("Girlie").parent().parent().within(() => {
+    HomePage.addToBasketButton.click();
+  });
 
-    // Create scenario - Add payment option
-    // Click on Account
-    // Click on Orders & Payment
-    // Click on My payment options
-    // Create page object - SavedPaymentMethodsPage
-    // Click Add new card
-    // Fill in Name
-    // Fill in Card Number
-    // Set expiry month to 7
-    // Set expiry year to 2090
-    // Click Submit button
-    // Validate that the card shows up in the list
+  
+  HomePage.yourBasketButton.click();
+
+  
+  HomePage.checkoutButton.click();
+  HomePage.addressCard.click();
+  HomePage.addressContinueButton.click();
+
+  HomePage.deliveryOption.click();
+  HomePage.deliveryContinueButton.click();
+
+  HomePage.paymentCard.click();
+  HomePage.paymentContinueButton.click();
+
+  HomePage.placeOrderButton.click();
+
+  
+  HomePage.confirmationMessage.should("be.visible");
+});
+
+it("Add address", () => {
+  HomePage.accountButton.click();
+  HomePage.ordersAndPaymentButton.click();
+  HomePage.mySavedAddressesButton.click();
+
+  HomePage.addNewAddressButton.click();
+
+  HomePage.countryField.type("Turkey");
+  HomePage.nameField.type("Ahmet Yılmaz");
+  HomePage.mobileNumberField.type("05551234567");
+  HomePage.zipCodeField.type("34000");
+  HomePage.addressField.type("Test Mah. Cypress Sok. No:1");
+  HomePage.cityField.type("Istanbul");
+  HomePage.stateField.type("Marmara");
+
+  HomePage.submitAddressButton.click();
+
+  HomePage.savedAddresses.should("contain.text", "Ahmet Yılmaz");
+});
+
+
+it("Add payment option", () => {
+  HomePage.accountButton.click();
+  HomePage.ordersAndPaymentButton.click();
+  HomePage.myPaymentOptionsButton.click();
+
+  HomePage.addNewCardButton.click();
+
+  HomePage.cardNameField.type("Ahmet Yılmaz");
+  HomePage.cardNumberField.type("1234123412345678");
+
+  HomePage.expiryMonthSelect.click();
+  HomePage.expiryDropdownOptions.contains("7").click();
+
+  HomePage.expiryYearSelect.click();
+  HomePage.expiryDropdownOptions.contains("2090").click();
+
+  HomePage.submitCardButton.click();
+
+  HomePage.savedCards.should("contain.text", "Ahmet Yılmaz");
+});
+
   });
 });
--- cypress/pageObjects/HomePage.js
+++ cypress/pageObjects/HomePage.js
@@ -66,5 +66,152 @@ export class HomePage extends BasePage {
   static get reviewComment() {
     return cy.get("mat-expansion-panel .review-text");
   }
+  static get reviewTextArea() {
+    return cy.get("textarea[aria-label='Text field to review a product']");
+  }
+
+  static get submitReviewButton() {
+    return cy.get("button[aria-label='Submit the review']");
+  }
+  static get itemsPerPageDropdown() {
+    return cy.get("mat-select[aria-label='Items per page:']");
+  }
+
+  static get itemsPerPageOptions() {
+    return cy.get("mat-option");
+  }
+  static get addToBasketButton() {
+    return cy.get("button[aria-label='Add to Basket']");
+  }
+
+  static get yourBasketButton() {
+    return cy.get("[aria-label='Show the shopping cart']");
+  }
+  static get checkoutButton() {
+    return cy.get("button#checkoutButton");
+  }
+
+  static get addressCard() {
+    return cy.contains("mat-card", "United Fakedom");
+  }
+
+  static get addressContinueButton() {
+    return cy.get("button[aria-label='Proceed to payment selection']");
+  }
+
+  static get deliveryOption() {
+    return cy.contains("mat-row", "Standard Delivery").find("mat-radio-button");
+  }
+
+  static get deliveryContinueButton() {
+    return cy.get("button[aria-label='Proceed to delivery method selection']");
+  }
+
+  static get paymentCard() {
+    return cy.contains("mat-radio-button", "5678");
+  }
+
+  static get paymentContinueButton() {
+    return cy.get("button[aria-label='Proceed to review']");
+  }
+
+  static get placeOrderButton() {
+    return cy.get("button[aria-label='Place your order and pay']");
+  }
+
+  static get confirmationMessage() {
+    return cy.get(".confirmation").contains("Thank you for your purchase!");
+  }
+
+  // Add Address elements
+  static get ordersAndPaymentButton() {
+    return cy.get("button[aria-label='Show Orders and Payment Menu']");
+  }
+
+  static get mySavedAddressesButton() {
+    return cy.get("[routerlink='/address/list']");
+  }
+
+  static get addNewAddressButton() {
+    return cy.get("[aria-label='Add a new address']");
+  }
+
+  static get countryField() {
+    return cy.get("input[data-placeholder='Please provide a country.']");
+  }
+
+  static get nameField() {
+    return cy.get("input[data-placeholder='Please provide a name.']");
+  }
+
+  static get mobileNumberField() {
+    return cy.get("input[data-placeholder='Please provide a mobile number.']");
+  }
+
+  static get zipCodeField() {
+    return cy.get("input[data-placeholder='Please provide a ZIP code.']");
+  }
+
+  static get addressField() {
+    return cy.get("textarea[data-placeholder='Please provide an address.']");
+  }
+
+  static get cityField() {
+    return cy.get("input[data-placeholder='Please provide a city.']");
+  }
+
+  static get stateField() {
+    return cy.get("input[data-placeholder='Please provide a state.']");
+  }
+
+  static get submitAddressButton() {
+    return cy.get("button#submitButton");
+  }
+
+  static get savedAddresses() {
+    return cy.get("mat-card.mat-card");
+  }
+
+  static get myPaymentOptionsButton() {
+    return cy.get("[routerlink='/payment/options']");
+  }
+
+  static get addNewCardButton() {
+    return cy.get("[aria-label='Add a new card']");
+  }
+
+  static get cardNameField() {
+    return cy.get("input[data-placeholder='Please enter the name on your card']");
+  }
+
+  static get cardNumberField() {
+    return cy.get("input[data-placeholder='Please enter your card number']");
+  }
+
+  static get expiryMonthSelect() {
+    return cy.get("mat-select[name='month']");
+  }
+
+  static get expiryYearSelect() {
+    return cy.get("mat-select[name='year']");
+  }
+
+  static get expiryDropdownOptions() {
+    return cy.get("mat-option");
+  }
+
+  static get submitCardButton() {
+    return cy.get("button#submitButton");
+  }
+
+  static get savedCards() {
+    return cy.get("mat-card.mat-card");
+  }
 } 
+
+
+
+
+
+
 
